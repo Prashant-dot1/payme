5ad32310f9b8513e46beb2e22d1f3924b4da1e61
Prashant-dot1 <puriprashant14@gmail.com>
2025-03-06 00:10:08 +0530

    create transaction handler

--- src/core/api/commands.rs
+++ src/core/api/commands.rs
@@ -1,12 +1,28 @@
-use serde::Deserialize;
+use axum::Json;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+use crate::core::models::TransactionStatus;
 
 /*request payload types - this is from the user*/
 #[derive(Deserialize)]
 pub struct CreateTransactionRequest {
     amount: u64,
     currency: String,
-    payment_method_id: String,
+    merchant_id: String,
     customer_id: String
 }
 
 /*response payload types*/
+
+#[derive(Serialize)]
+pub struct CreateTransactionResponse {
+    id : Uuid,
+    status: TransactionStatus
+}
+
+/* Handlers */
+pub async fn create_transaction(Json(req_payload): Json<CreateTransactionRequest>) -> Json<CreateTransactionResponse> {
+
+    todo!("integration with the command service")
+}
